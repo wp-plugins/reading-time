eecf46717c879f801a26f0d707ce118c7369b492
formikaio <formikaio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-06 17:19:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/reading-time/trunk@329435 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- reading_time.php
+++ reading_time.php
@@ -4,7 +4,7 @@ Plugin Name: Reading Time
 Plugin URI: http://www.whiletrue.it
 Description: Shows the suggested reading time of web content. To use it inside a post, create a custom field named "readingtime" and give it the number of seconds suggested (e.g. 150).
 Author: WhileTrue
-Version: 1.1.0
+Version: 1.2.0
 Author URI: http://www.whiletrue.it
 */
 
@@ -36,8 +36,10 @@ function reading_time ($content) {
 		}
 		$reading_time_options = explode('|||',get_option('reading_time'));
 
+		// FIRST TRY TO GET ESTIMATED TIME FROM CUSTOM VALUES		
 		$tempo = get_post_custom_values('readingtime');
 		if (!is_numeric($tempo[0])) {
+			// CALCULATE ESTIMATED TIME		
 			$tempo[0] = round(str_word_count(strip_tags($content))*60/$reading_time_options[1]);
 			if (!is_numeric($tempo[0])) {
 				return $content;
@@ -45,34 +47,42 @@ function reading_time ($content) {
 		}
 		$default_reading_time = $tempo[0];
 
-		$barcolor = $reading_time_options[2];
-		if ($barcolor=='') $barcolor = 'red';
-		
+		// COMPOSE TEXT MESSAGE
 		$text = $reading_time_options[0];
 		if ($text=='') $text = 'I think you will spend SSSS seconds reading this post';
 		$text = str_replace('SSSS', $default_reading_time, $text);
-
-		wp_enqueue_script('jquery');
-		$post_id = get_the_ID();
-		$out = '
-			<style>
-				.readingtime_border { border:1px solid black;width:250px;height:10px; }
-				.readingtime_bar { background-color:'.$barcolor.';width:0px;height:10px; }
-			</style>
-			
-			<p>'.$text.'</p>
-			<p>
-				<div class="readingtime_border">
-					<div id="readingtime_bar_in_'.$post_id.'" class="readingtime_bar"></div>
-				</div>
-			</p>
 		
-			<script type="text/javascript">
-			jQuery("#readingtime_bar_in_'.$post_id.'").animate({
-			    width: "100%"
-			}, '.$default_reading_time.'*1000);
-			</script>
-			';
+		$out = '<p>'.stripslashes($text).'</p>';
+		
+		// CHECK AGAINST 'NO' FOR BACKWARD COMPATIBILITY
+		if ($reading_time_options[4]!='no') {	
+			// DISPLAY PROGRESS BAR
+
+			wp_enqueue_script('jquery');
+			$post_id = get_the_ID();
+	
+			$barcolor = $reading_time_options[2];
+			if ($barcolor=='') $barcolor = 'red';
+
+			$out .= '
+				<style>
+					.readingtime_border { border:1px solid black;width:250px;height:10px; }
+					.readingtime_bar { background-color:'.$barcolor.';width:0px;height:10px; }
+				</style>
+				
+				<p>
+					<div class="readingtime_border">
+						<div id="readingtime_bar_in_'.$post_id.'" class="readingtime_bar"></div>
+					</div>
+				</p>
+			
+				<script type="text/javascript">
+				jQuery("#readingtime_bar_in_'.$post_id.'").animate({
+				    width: "100%"
+				}, '.$default_reading_time.'*1000);
+				</script>
+				';
+		}
 		
 	if ($reading_time_options[3]=='below') {
 		return $content.$out;
@@ -94,8 +104,8 @@ function reading_time_options () {
 	or isset($_POST['reading_time_bar_color']) or isset($_POST['reading_time_position'])) {
 
         update_option( 'reading_time', 
-			$_POST['reading_time_text'].'|||'.$_POST['reading_time_speed'].'|||'
-			.$_POST['reading_time_bar_color'].'|||'.$_POST['reading_time_position'] );
+			esc_html($_POST['reading_time_text'].'|||'.$_POST['reading_time_speed'].'|||'
+			.$_POST['reading_time_bar_color'].'|||'.$_POST['reading_time_position'].'|||'.$_POST['reading_time_bar_display']));
 
 		// Put an settings updated message on the screen
 
@@ -112,7 +122,7 @@ function reading_time_options () {
 		//OPTION NOT IN DATABASE, SO WE INSERT DEFAULT VALUES
 		add_option('reading_time', 
 			'The estimated reading time for this post is SSSS seconds'.'|||'.'200'.'|||'
-			.'blue'.'|||'.'above');
+			.'blue'.'|||'.'above'.'|||'.'yes');
 		$option_string = get_option('reading_time');
 	}
 
@@ -120,6 +130,9 @@ function reading_time_options () {
 
 	$sel_above = ($reading_time_options[3]=='above') ? 'selected="selected"' : '';
 	$sel_below = ($reading_time_options[3]=='below') ? 'selected="selected"' : '';
+
+	$sel_bar_yes = ($reading_time_options[4]=='yes') ? 'selected="selected"' : '';
+	$sel_bar_no  = ($reading_time_options[4]=='no')  ? 'selected="selected"' : '';
 
     // SETTINGS FORM
     ?>
@@ -130,27 +143,33 @@ function reading_time_options () {
 
 	<table>
 	<tr><td valign="top"><?php _e("Text", 'menu-test' ); ?>:</td>
-	<td><input type="text" name="reading_time_text" value="<?php echo $reading_time_options[0]; ?>" size="100"><br />
-	<?php _e("Use 'SSSS' as a placeholder for seconds, e.g. 'The estimated reading time for this post is SSSS seconds'", 'menu-test' ); ?>
+	<td><input type="text" name="reading_time_text" value="<?php echo stripslashes($reading_time_options[0]); ?>" size="100"><br />
+	<span class="description"><?php _e("Use 'SSSS' as a placeholder for seconds, e.g. 'The estimated reading time for this post is SSSS seconds'", 'menu-test' ); ?></span><br />
 	<br /></td></tr>
 
 	<tr><td valign="top"><?php _e("Speed", 'menu-test' ); ?>:</td>
-	<td><input type="text" name="reading_time_speed" value="<?php echo $reading_time_options[1]; ?>" size="20"><br />
-	<?php _e("E.g. 250 for fast readers, 150 for slow readers", 'menu-test' ); ?>
+	<td><input type="text" name="reading_time_speed" value="<?php echo absint($reading_time_options[1]); ?>" size="20"><br />
+	<span class="description"><?php _e("E.g. 250 for fast readers, 150 for slow readers", 'menu-test' ); ?></span><br />
 	<br /></td></tr>
 
-	<tr><td valign="top"><?php _e("Bar color", 'menu-test' ); ?>:</td>
+	<tr><td valign="top"><?php _e("Progress bar color", 'menu-test' ); ?>:</td>
 	<td><input type="text" name="reading_time_bar_color" value="<?php echo $reading_time_options[2]; ?>" size="20"><br />
-	<?php _e("E.g. 'blue', '#006699'", 'menu-test' ); ?>
+	<span class="description"><?php _e("E.g. 'blue', '#006699'", 'menu-test' ); ?></span><br />
 	<br /></td></tr>
 
 	<tr><td valign="top"><?php _e("Position", 'menu-test' ); ?>:</td>
 	<td><select name="reading_time_position">
 		<option value="above" <?php echo $sel_above; ?> > <?php _e('above the post', 'menu-test' ); ?></option>
 		<option value="below" <?php echo $sel_below; ?> > <?php _e('below the post', 'menu-test' ); ?></option>
-		</select> 
+		</select><br /> 
 	<br /></td></tr>
 
+	<tr><td valign="top"><?php _e("Progress bar display", 'menu-test' ); ?>:</td>
+	<td><select name="reading_time_bar_display">
+		<option value="yes" <?php echo $sel_bar_yes; ?> > <?php _e('yes', 'menu-test' ); ?></option>
+		<option value="no"  <?php echo $sel_bar_no; ?> > <?php _e('no',  'menu-test' ); ?></option>
+		</select><br /> 
+	<br /></td></tr>
 	</table>
 	<hr />
 	<p class="submit">
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.whiletrue.it/
 Tags: reading time, estimate time, reading, time, word count
 Requires at least: 2.9+
 Tested up to: 3.0
-Stable tag: 1.1.0
+Stable tag: 1.2.0
 
 Reading Time shows the estimated reading time and puts an animated progress bar inside the post. 
 
@@ -12,11 +12,11 @@ Reading Time shows the estimated reading time and puts an animated progress bar 
 Reading Time shows the estimated reading time of the post. 
 The calculation is automatic; a custom value (in seconds) for a single post can be inserted using a Custom Field named "readingtime".
 
-For more informations: http://www.whiletrue.it/
+For more informations: http://www.whiletrue.it/en/projects/wordpress/17-reading-time-per-wordpress.html
 
 == Installation ==
 1. Upload `reading_time.php` and `unisntall.php` into the `/wp-content/plugins/reading_time` directory
-2. (optional) Set your favourite values in the `Settings->Reading Time` menu in Wordpress
+2. Set your favourite values in the `Settings->Reading Time` menu in Wordpress
 3. Activate the plugin through the `Plugins` menu in WordPress
 4. Enjoy!
 
@@ -30,14 +30,18 @@ The value for speed should be chosen between a span of 150 words per minute (sli
 
 
 == Features In Next Release ==
-* Display setting: always / only when the `readingtime` custom tag is set
+* Display setting: always / only for selected tags or categories / only when the `readingtime` custom tag is set
 
 
 == Changelog ==
 
+= 1.2.0 =
+* New progress bar display setting
+* Settings sanitization
+
 = 1.1.0 =
 * Default settings value
-* Position setting: above or below the post
+* New position setting: above or below the post
 * Uninstall function
 
 = 1.0.0 =
@@ -45,6 +49,9 @@ Initial version
 
 
 == Upgrade Notice == 
+
+= 1.2.0 =
+No upgrade issues known
 
 = 1.1.0 =
 No upgrade issues known

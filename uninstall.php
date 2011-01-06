ac8981766526fcb57de000a553391039a338a6c3
formikaio <formikaio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-06 17:19:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/reading-time/tags/1.2.0@329435 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Reading Time
+Plugin URI: http://www.whiletrue.it
+Description: Shows the suggested reading time of web content. To use it inside a post, create a custom field named "readingtime" and give it the number of seconds suggested (e.g. 150).
+Author: WhileTrue
+Version: 1.2.0
+Author URI: http://www.whiletrue.it
+*/
+
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2, 
+    as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+delete_option('reading_time');
